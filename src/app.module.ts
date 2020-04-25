240c31d889fb140613e64b26a7eff2d580438b55
Jim Strother <jim.strother@gmail.com>
2020-04-25 14:46:25 -0700

    Added: module, controllers, services, and others for items

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
+require('dotenv').config();
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ItemsModule } from './items/items.module';
 
 @Module({
-  imports: [],
+  imports: [ItemsModule, MongooseModule.forRoot(process.env.DATABASE_URI)],
   controllers: [AppController],
   providers: [AppService],
 })
