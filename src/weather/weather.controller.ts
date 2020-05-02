5adaf2ffe94054048600f7bd292174783dff32e7
Jim Strother <jim.strother@gmail.com>
2020-05-02 11:10:00 -0700

    Tested & Confirmed: weather api working as expected

--- src/weather/weather.controller.ts
+++ src/weather/weather.controller.ts
@@ -6,13 +6,6 @@ export class WeatherController {
   constructor(private readonly weatherService: WeatherService) {}
 
   @Get(':city')
-  /** the name in `@Param()` needs to match the parameter name in the `@Get()` 
-   * so in this case ":city" being in the `@Get()` means that req.params has
-   * the shape of { city: string }. By using `@Params('city') we are essentially
-   * using `req.params['city'], meaning we pull the correct property. To test
-   * this, you can always use `@Param()` without the string, and see what
-   * is put into the resulting object
-   */ 
   getWeather(@Param('city') city: string): object {
     return this.weatherService.forCity(city);
   }
